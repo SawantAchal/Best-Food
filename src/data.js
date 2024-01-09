1e3c6a6f90548176b386d741cc99755cce8acc82
SawantAchal <achals1718@gmail.com>
2024-01-09 17:36:34 +0530

    add required imagges and components to app,js fiile

--- src/data.js
+++ src/data.js
@@ -1,78 +1,155 @@
+import img from './Assets/pimg1.jpg'
+
 export const data =[
     {
         id:1,
         name:'Double Cheeseburger',
         category:'burger',
-        image:'',
+        image:img,
         prices:'$$$$'
     },
     {
         id:2,
         name:'Double Cheeseburger',
         category:'burger',
-        image:'',
+        image:img,
         prices:'$$$'
     },
     {
         id:3,
         name:'Double Cheeseburger',
         category:'burger',
-        image:'',
+        image:img,
         prices:'$'
     },
     {
         id:4,
         name:'Double Cheeseburger',
         category:'burger',
-        image:'',
+        image:img,
         prices:'$$$$'
     },
     {
         id:5,
         name:'Double Cheeseburger',
         category:'pizza',
-        image:'',
+        image:img,
         prices:'$$$$'
     },
     {
         id:6,
         name:'Double Cheeseburger',
         category:'pizza',
-        image:'',
+        image:img,
         prices:'$$'
     },
     {
         id:7,
         name:'Double Cheeseburger',
         category:'pizza',
-        image:'',
+        image:img,
         prices:'$$$$'
     },
     {
         id:8,
         name:'Double Cheeseburger',
         category:'pizza',
-        image:'',
+        image:img,
         prices:'$$$'
     },
     {
         id:9,
         name:'Double Cheeseburger',
         category:'pizza',
-        image:'',
+        image:img,
         prices:'$$$$'
     },    {
         id:10,
         name:'Double Cheeseburger',
         category:'burger',
-        image:'',
+        image:img,
         prices:'$$'
     },
     {
         id:11,
         name:'Double Cheeseburger',
         category:'pizza',
-        image:'',
+        image:img,
         prices:'$$'
+    },
+    {
+        id:12,
+        name:'Double Cheeseburger',
+        category:'salad',
+        image:img,
+        prices:'$$$$'
+    },
+    {
+        id:13,
+        name:'Double Cheeseburger',
+        category:'chicken',
+        image:img,
+        prices:'$$'
+    },
+    {
+        id:14,
+        name:'Double Cheeseburger',
+        category:'chicken',
+        image:img,
+        prices:'$$$'
+    },
+    {
+        id:15,
+        name:'Double Cheeseburger',
+        category:'salad',
+        image:img,
+        prices:'$$'
+    },
+    {
+        id:16,
+        name:'Double Cheeseburger',
+        category:'chicken',
+        image:img,
+        prices:'$$'
+    },
+    {
+        id:17,
+        name:'Double Cheeseburger',
+        category:'chicken',
+        image:img,
+        prices:'$$'
+    },
+]
+
+export const categories =[
+    {
+        id:1,
+        name:'indian',
+        image:''
+    },
+    {
+        id:2,
+        name:'india',
+        image:''
+    },
+    {
+        id:3,
+        name:'indi',
+        image:''
+    },
+    {
+        id:4,
+        name:'ind',
+        image:''
+    },
+    {
+        id:5,
+        name:'in',
+        image:''
+    },
+    {
+        id:6,
+        name:'i',
+        image:''
     },
 ]
