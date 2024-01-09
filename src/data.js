e65e2a8a04d7e21baf2804ec14f4e8d72cdb433a
SawantAchal <achals1718@gmail.com>
2024-01-09 16:07:38 +0530

    created json data file to fecth and filter data

---
+++ src/data.js
@@ -0,0 +1,78 @@
+export const data =[
+    {
+        id:1,
+        name:'Double Cheeseburger',
+        category:'burger',
+        image:'',
+        prices:'$$$$'
+    },
+    {
+        id:2,
+        name:'Double Cheeseburger',
+        category:'burger',
+        image:'',
+        prices:'$$$'
+    },
+    {
+        id:3,
+        name:'Double Cheeseburger',
+        category:'burger',
+        image:'',
+        prices:'$'
+    },
+    {
+        id:4,
+        name:'Double Cheeseburger',
+        category:'burger',
+        image:'',
+        prices:'$$$$'
+    },
+    {
+        id:5,
+        name:'Double Cheeseburger',
+        category:'pizza',
+        image:'',
+        prices:'$$$$'
+    },
+    {
+        id:6,
+        name:'Double Cheeseburger',
+        category:'pizza',
+        image:'',
+        prices:'$$'
+    },
+    {
+        id:7,
+        name:'Double Cheeseburger',
+        category:'pizza',
+        image:'',
+        prices:'$$$$'
+    },
+    {
+        id:8,
+        name:'Double Cheeseburger',
+        category:'pizza',
+        image:'',
+        prices:'$$$'
+    },
+    {
+        id:9,
+        name:'Double Cheeseburger',
+        category:'pizza',
+        image:'',
+        prices:'$$$$'
+    },    {
+        id:10,
+        name:'Double Cheeseburger',
+        category:'burger',
+        image:'',
+        prices:'$$'
+    },
+    {
+        id:11,
+        name:'Double Cheeseburger',
+        category:'pizza',
+        image:'',
+        prices:'$$'
+    },
+]
