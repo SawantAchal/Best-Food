d60ef5f837117c4ca6dc3c4fc3a55809c842bf8f
SawantAchal <achals1718@gmail.com>
2024-01-10 00:34:06 +0530

    renew price filter by range

--- src/Components/Food.jsx
+++ src/Components/Food.jsx
@@ -3,6 +3,8 @@ import {data} from '../data'
 
 const Food = () => {
     const [food , setFood] = useState(data)
+    const [minPrice, setMinPrice] = useState('');
+    const [maxPrice, setMaxPrice] = useState('');
     // console.log(data)
 
     // filter by type
@@ -14,11 +16,33 @@ const Food = () => {
         )
     }
 
-    // filter by price
-    const filterPrice = (prices) =>{
+    // // filter by price
+    // const filterPrice = (prices) =>{
+    //     setFood(
+    //         data.filter((item) => {
+    //             return item.prices === prices
+    //         })
+    //     )
+    // }
+
+    // filter by price range
+    const filterPriceRange = () => {
         setFood(
             data.filter((item) => {
-                return item.prices === prices
+                const productPrice = parseFloat(item.prices.replace(/[^\d.]/g, '')); // Remove non-numeric characters
+                    return (
+                        (minPrice === '' || productPrice >= parseFloat(minPrice)) &&
+                        (maxPrice === '' || productPrice <= parseFloat(maxPrice))
+                    );
+            }  )
+        );
+    };
+
+    // filter by type
+    const filterTypes = (type) =>{
+        setFood(
+            data.filter((item) => {
+                return item.type === type
             })
         )
     }
@@ -39,8 +63,16 @@ const Food = () => {
                     <button onClick={() => filterType('chicken')} className='border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white m-1'>Chicken</button>
                 </section>
             </section>
-            {/* <filter prices */}
+            {/* <filter types */}
             <section>
+                <p className='font-bold text-gray-700'>Filter Types</p>
+                <section className='flex justify-between max-w-[390px] w-full'>
+                    <button onClick={() => filterTypes('veg')} className='border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white m-1'>Veg</button>
+                    <button onClick={() => filterTypes('non-veg')} className='border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white m-1'>Non-Veg</button>
+                </section>
+            </section>
+            {/* <filter prices */}
+            {/* <section>
                 <p className='font-bold text-gray-700'>Filter price</p>
                 <section className='flex justify-between max-w-[390px] w-full'>
                     <button onClick={() => filterPrice('$')} className='border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white m-1'>$</button>
@@ -48,10 +80,19 @@ const Food = () => {
                     <button onClick={() => filterPrice('$$$')} className='border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white m-1'>$$$</button>
                     <button onClick={() => filterPrice('$$$$')} className='border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white m-1'>$$$$</button>
                 </section>
+            </section> */}
+
+            <section>
+                <p className='font-bold text-gray-700'>Filter price range</p>
+                <section className='flex justify-between max-w-[390px] w-full'>
+                    <input type='number' placeholder='Min Price' value={minPrice} onChange={(e) => setMinPrice(e.target.value)} className='border-orange-600 text-orange-600  p-1 w-1/2' />
+                    <input type='number' placeholder='Max Price' value={maxPrice} onChange={(e) => setMaxPrice(e.target.value)} className='border-orange-600 p-1 w-1/2' />
+                    <button onClick={filterPriceRange} className='border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white m-1'>Apply</button>
+                </section>
             </section>
         </section>
         {/* display food */}
-        <section className='grid grid-cols-1 lg:grid-cols-4 gap-6 pt-4'>
+        <section className='grid grid-cols-1 lg:grid-cols-4 gap-6 pt-4 md:grid-cols-2'>
             {
                 food.map((item , index) => {
                     return(
@@ -59,7 +100,8 @@ const Food = () => {
                             <img src={item.image} alt={item.name} className='w-full h-[200px] object-cover rounded-t-lg'/>
                             <section className='flex justify-between px-2 py-4 '>
                                 <p className='font-bold'>{item.name}</p>
-                                <p>{item.category}</p>
+                                {/* <p>{item.category}</p>
+                                <p>{item.type}</p> */}
                                 <p className=''><span className='bg-orange-500 text-white p-1 rounded-full'>{item.prices}</span></p>
                             </section>
                         </section>
